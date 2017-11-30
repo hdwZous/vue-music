39adc87f9983c2c35aae675eec60b39f2d56f9e5
hedongwei <891546646@qq.com>
2017-11-30 09:45:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello word
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  #app 
+    color:$color-theme 
+</style>
